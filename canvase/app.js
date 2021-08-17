804908ac7c1defcff635b4b45069b4cebc09055f
김성은 <kse012050@naver.com>
2021-08-17 13:59:20 +0900

    canvas 만드는 중이다

--- canvase/app.js
+++ canvase/app.js
@@ -1,2 +1,19 @@
-// class App(){
-// }
+class App{
+    constructor(){
+        this.canvas = document.querySelector('#mainCanvas');
+        this.ctx = canvas.getContext('2d');
+
+        resize();
+    }
+
+    resize(){
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        
+    }
+}
+
+window.onload = () => {
+    new App();
+}
