db101b11cea8cc21483bea16ad647a3a04ad0364
김성은 <kse012050@naver.com>
2021-09-06 03:09:36 +0900

    supports_FAQs page fin

--- js/common.js
+++ js/common.js
@@ -1,7 +1,31 @@
 $(document).ready(function(){
+    subMenu();
+    tab(); 
+
+    FAQsTab();
+})
+
+function subMenu(){
     $('.subPage header > div nav > ul').hover(function(){
         $('.subPage header > div nav > ul > li ul').stop().slideDown();
     },function(){
         $('.subPage header > div nav > ul > li ul').stop().slideUp();
     })
-})
+}
+
+function tab(){
+    var tabIdx = 0;
+    $('.tabBtn li button').click(function(){
+        $(this).parent().addClass('active').siblings().removeClass('active')
+        tabIdx = $(this).parent().index();
+        // $('.tabArea > li').eq(tabIdx).addClass('active').siblings().removeClass('active');
+        $('.tabArea > li').eq(tabIdx).fadeIn().siblings().hide();
+    })
+}
+
+function FAQsTab(){
+    $('.FAQsArea .contentArea .tabArea > li ul li > button').click(function(){
+        $(this).parent().toggleClass('active');
+        $(this).next().slideToggle();
+    })
+}
