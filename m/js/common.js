13bd18c349030d62e4d24bf4cc720d7f72c62376
김성은 <kse012050@naver.com>
2021-09-10 08:00:43 +0900

    rem 변경 완료 모바일 fin

--- m/js/common.js
+++ m/js/common.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     menu();
+    tab();
+    FAQsTab();
 });
 
 function menu(){
@@ -10,6 +12,23 @@ function menu(){
 
     $('header > div nav > ul > li > a').click(function(e){
         e.preventDefault();
+        $(this).next().slideToggle();
+    })
+}
+
+function tab(){
+    var tabIdx = 0;
+    $('.tabBtn li button').click(function(){
+        $(this).parent().addClass('active').siblings().removeClass('active')
+        tabIdx = $(this).parent().index();
+        // $('.tabArea > li').eq(tabIdx).addClass('active').siblings().removeClass('active');
+        $('.tabArea > li').eq(tabIdx).fadeIn().siblings().hide();
+    })
+}
+
+function FAQsTab(){
+    $('.FAQsArea .contentArea .tabArea > li ul li > button').click(function(){
+        $(this).parent().toggleClass('active');
         $(this).next().slideToggle();
     })
 }
